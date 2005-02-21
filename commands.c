cd58cdd56d8b6fb4c5725bc1a80aa204b70aa843
leitec <leitec>
2005-02-21 03:55:51 +0000

    *** empty log message ***

--- commands.c
+++ commands.c
@@ -18,12 +18,11 @@ irclib_join(void *handle, char *channel)
 {
 	pkt_t          *pkt;
 
-	pkt = pkt_init(5 + strlen(channel) + 2);
+	pkt = pkt_init(5 + strlen(channel));
 
 	pkt_addstr(pkt, "JOIN ");
 	pkt_addstr(pkt, channel);
-	pkt_addstr(pkt, "\r\n");
 
-	sendPkt(handle, pkt);
+	send_cmdpkt(handle, pkt);
 	pkt_free(pkt);
 }
